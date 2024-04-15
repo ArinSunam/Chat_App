84ea49c21ad2f4e530b5c5fcdd53e7ce3014c282
Grindmeow <thehumaneatermeow@gmail.com>
2024-04-15 17:02:17 +0545

    feat: major api integration

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import { useAuthContext } from '../../context/AuthContext'
+import useConversation from '../../zustand/useConversation';
+import { extractTime } from '../../utils/extractTimes';
 
-const Message = () => {
+const Message = ({ message }) => {
+
+  const { authUser } = useAuthContext();
+
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId == authUser._id;
+  const formattedTime = extractTime(message.createdAt)
+
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? 'bg-blue-500' : '';
+
   return (
-    <div className='chat chat-end'>
+    <div className={`chat  ${chatClassName}`}>
       <div className='chat-image avatar'>
         <div className='w-10 rounded-full'>
-          <img src="https://avatar.iran.liara.run/public/girl?username=Myau" alt="Tailwnd CSS chat bubble component" />
+          <img src={profilePic} alt="Tailwnd CSS chat bubble component" />
         </div>
       </div>
 
-      <div className='chat-bubble text-white bg-blue-500'>Yo what's upp</div>
-      <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'> 12:42</div>
+      <div className={`chat-bubble text-white  ${bubbleBgColor}`}>{message.message}</div>
+      <div className='pt-1 chat-footer opacity-50 text-xs flex gap-1 items-center'> {formattedTime}</div>
 
     </div>
   )
