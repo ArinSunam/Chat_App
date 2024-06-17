0133b95fc664cee02376e597377b60abc6a48166
Grindmeow <thehumaneatermeow@gmail.com>
2024-06-17 23:56:00 +0545

    forDeployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
@@ -10,6 +11,7 @@ import { app, server } from './socket/socket.js';
 //variables
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 //config
 dotenv.config();
@@ -20,7 +22,12 @@ app.use(express.json()); //to parse the incoming requests with JSON payloads(fro
 app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
-app.use("/api/users", userRoutes)
+app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 
 server.listen(PORT, () => {
